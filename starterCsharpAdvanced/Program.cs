62594d165910cfb3bf54dec4aca47a5498d6b3d9
robmarano <robmarano@gmail.com>
2014-03-20 19:20:15 -0400

    Demonstrating first handled exceptions.

---
+++ starterCsharpAdvanced/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace starterCsharpAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Argument["+i+"] = "+args[i]);
+                try
+                {
+                    string text = System.IO.File.ReadAllText(args[i]);
+                    Console.WriteLine(text);
+                }
+                catch (Exception ex)
+                {
+                    // Console.WriteLine(ex.ToString());n
+                }
+            }
+
+        }
+    }
+}
