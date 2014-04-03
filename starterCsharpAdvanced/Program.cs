eacb48093437b18eb2ed9c9bb80c4290472624d5
robmarano <robmarano@gmail.com>
2014-04-03 19:16:01 -0400

    Using delegate ThreadStart to indicate where the execution logic begins for a Thread.

--- starterCsharpAdvanced/Program.cs
+++ starterCsharpAdvanced/Program.cs
@@ -7,19 +7,15 @@ namespace starterCsharpAdvanced
     {
         static void Main()
         {
-            Thread t = new Thread(Go);
-            t.Start();
-            Go();
-            Console.WriteLine("Main() thread has ended.");
-            t.Join();
-            Console.WriteLine("Thread t has ended.");
+            Thread t = new Thread(new ThreadStart(Go));
+
+            t.Start();   // Run Go() on the new thread.
+            Go();        // Simultaneously run Go() in the main thread.
         }
 
         static void Go()
         {
-            for (int i = 0; i < 1000; i++) Console.Write("y");
-            Console.WriteLine("\n...zzz...");
-            Thread.Sleep(1500); // sleep for 1500 milliseconds
+            Console.WriteLine("hello!");
         }
     }
 }
