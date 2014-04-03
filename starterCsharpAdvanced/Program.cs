c46c0067daecdf2a30d881afee0a31febe1fcc06
robmarano <robmarano@gmail.com>
2014-04-03 19:27:31 -0400

    Demonstrate how to use exception handling with Thread.

--- starterCsharpAdvanced/Program.cs
+++ starterCsharpAdvanced/Program.cs
@@ -5,18 +5,25 @@ namespace starterCsharpAdvanced
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Thread.CurrentThread.Name = "main";
-            Thread worker = new Thread(Go);
-            worker.Name = "worker";
-            worker.Start();
-            Go();
+            new Thread(Go).Start();
         }
 
         static void Go()
         {
-            Console.WriteLine("Hello from " + Thread.CurrentThread.Name);
+            try
+            {
+                // ...
+                throw null;    // The NullReferenceException will get caught below
+                // ...
+            }
+            catch (Exception ex)
+            {
+                // Typically log the exception, and/or signal another thread
+                // that we've come unstuck
+                // ...
+            }
         }
     }
 }
