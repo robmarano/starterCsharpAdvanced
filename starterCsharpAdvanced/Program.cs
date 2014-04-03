407cb260b1af321798f13186e262cf4165bbd329
robmarano <robmarano@gmail.com>
2014-04-03 19:58:58 -0400

    Exercise to twiddle values between two threads.

--- starterCsharpAdvanced/Program.cs
+++ starterCsharpAdvanced/Program.cs
@@ -5,25 +5,53 @@ namespace starterCsharpAdvanced
 {
     class Program
     {
-        public static void Main()
+        public static int t1Val {get;set;}
+        static readonly object locker1 = new object();
+        public static int t2Val {get;set;}
+        static readonly object locker2 = new object();
+
+        static void Main()
         {
-            new Thread(Go).Start();
+            Thread.CurrentThread.Name = "main";
+            t1Val = 1;
+            Thread t1 = new Thread(() => Go());
+            t1.Name = "thread1";
+            t2Val = 2;
+            Thread t2 = new Thread(() => Go());
+            t2.Name = "thread2";
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
         }
 
         static void Go()
         {
-            try
+            int temp;
+            for (int y = 0; y < 10000; y++)
             {
-                // ...
-                throw null;    // The NullReferenceException will get caught below
-                // ...
+                lock (locker1)
+                {
+                    temp = t1Val;
+                    lock (locker2)
+                    {
+                        t1Val = t2Val;
+                        t2Val = temp;
+                    }
+                }
+                int valu = 0;
+                if (Thread.CurrentThread.Name == "thread1")
+                {
+                    valu = t1Val;
+                }
+                else if (Thread.CurrentThread.Name == "thread2")
+                {
+                    valu = t2Val;
+                }
+                //Console.Write(Thread.CurrentThread.Name + ":" + valu);
+                Console.Write(valu);
             }
-            catch (Exception ex)
-            {
-                // Typically log the exception, and/or signal another thread
-                // that we've come unstuck
-                // ...
-            }
+            Console.WriteLine("");
         }
     }
 }
