5122faa817a5dace6b02aa9f673081c3f8f370d1
robmarano <robmarano@gmail.com>
2014-05-08 16:17:58 -0400

    Added ChatServer project and ChatClient project. ChatClient is not fully functional yet. It's just a place holder. Need to keep as console application but have it split input display and output display.

---
+++ ChatServer/ChatServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServer chatServer = new ChatServer();
+        }
+    }
+}
