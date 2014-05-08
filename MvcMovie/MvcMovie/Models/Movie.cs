eaee8f2f44ba6bbdf3e4990709c447fb04978b52
robmarano <robmarano@gmail.com>
2014-05-08 18:33:14 -0400

    Added HelloWorld view and simple Movie.cs model. We have yet to tie it together with the Controller and View. Next commit will show how to connect the model to a SQL database via the .NET Entity Framework.

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    // The MovieDBContext class represents the Entity Framework movie database context,
+    // which handles fetching, storing, and updating Movie class instances in a database.
+    // The MovieDBContext derives from the DbContext base class provided by the Entity Framework.
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
